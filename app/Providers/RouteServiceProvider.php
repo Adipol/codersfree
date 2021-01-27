836181b15d941d7467d753ba0c208f59413f430b
george <adipol13@gmail.com>
2021-01-26 22:04:00 -0400

    10. Instalar la plantilla AdminLTE

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,8 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/dashboard';
+    //NOTE se elimino dashboard
+    public const HOME = '/';
 
     /**
      * The controller namespace for the application.
@@ -46,6 +47,11 @@ class RouteServiceProvider extends ServiceProvider
             Route::middleware('web')
                 ->namespace($this->namespace)
                 ->group(base_path('routes/web.php'));
+
+            Route::middleware('web')
+                ->prefix('admin')
+                ->namespace($this->namespace)
+                ->group(base_path('routes/admin.php'));
         });
     }
 
